9812e3efed96bc66b6977b303aab6114fe7af69a
Rahul Sikdar <sikdarrahul0@gmail.com>
2021-07-26 19:34:08 +0600

    complete

--- src/Components/Navbar/NavBar.js
+++ src/Components/Navbar/NavBar.js
@@ -1,14 +1,25 @@
 import React, { useContext } from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+import { toast, ToastContainer, Zoom } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { UserContext } from '../../App';
 import logo from '../../image&gif/brand-logo.svg';
 import './NavBar.css';
 
 const NavBar = () => {
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const signOut = () =>{
+        toast.dark( "sign out successful",{
+            autoClose: 2000,
+            transition: Zoom
+          });
+          setLoggedInUser({});
+    }
+    
     return (
         <section>
+            <ToastContainer />
              <Navbar className="navbar-design" fixed="top" expand="lg">
              <Navbar.Brand> <NavLink to="/"><img className="logo" src={logo} alt="brand-logo" /></NavLink></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -21,7 +32,7 @@ const NavBar = () => {
                     <NavLink exact to="/signup" className="nav-link"> Sign up</NavLink>
                     {
                     loggedInUser.username ?
-                   <NavLink to="/" onClick={() => setLoggedInUser({})} className="nav-link mr-4"> {loggedInUser.username} (sign out)</NavLink>
+                   <NavLink to="/" onClick={() => signOut() } className="nav-link mr-4"> {loggedInUser.username} (sign out)</NavLink>
                     :
                     <NavLink to="/login" className="nav-link mr-4">Log in</NavLink>
                 }
